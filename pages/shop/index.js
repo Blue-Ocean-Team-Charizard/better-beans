ad6378497017f27c0375866b3522fb08d1ea6a65
Isabell Lee <izzy.isabell@gmail.com>
2021-10-18 16:35:50 -0700

    incorporated Meta component for Head

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,13 +1,10 @@
-import Head from "next/head";
+import Meta from "../../components/Meta";
 import Shop from "../../components/Shop";
 
 export default function Shops() {
   return (
     <div>
-      <Head>
-        <title>Blue Ocean</title>
-        <meta name="keywords" content="Blue Ocean" />
-      </Head>
+      <Meta />
       <Shop />
     </div>
   );
