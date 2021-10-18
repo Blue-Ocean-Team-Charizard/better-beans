0ca401d6adb0f983a57b6e00567e183f6faf9c37
Ya Liu <sarahl0216@gmail.com>
2021-10-18 16:30:53 -0700

    able to get current geolocation in search

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,8 +1,43 @@
-export default function Search() {
-  return (
-    <div className="search">
-      <input type="text" id="search" placeholder="Search location" />
-      <button type="submit">GO</button>
-    </div>
-  );
+import React from 'react';
+
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: '',
+    };
+
+    this.getLocation = this.getLocation.bind(this);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    function success(pos) {
+      const crd = pos.coords;
+
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      // this.setState({ currentLocation: crd });
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <input type="text" id="search" placeholder="Search location" />
+        <button type="submit">GO</button>
+        <button onClick={this.getLocation}>search my location</button>
+      </div>
+    );
+  }
 }
---
+++ helpers/nearbySearch.js
@@ -0,0 +1,1 @@
+import APIKey from '../config/config';
