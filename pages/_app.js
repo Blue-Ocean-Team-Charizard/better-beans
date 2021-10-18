1512b38523e121e28e353c0dc13046947baa827a
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-10-18 11:36:06 -0700

    Getting started with firebase auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { AuthUserProvider } from '../firebase/auth_context';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>;
 }
 
-export default MyApp
+export default MyApp;
