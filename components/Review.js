1dc7ce50f2b8740887f1f90fdc82dcfba5cf57e1
Stormi <stormikhashimoto@gmail.com>
2021-10-19 12:25:37 -0700

    reviewer beans

--- components/Review.js
+++ components/Review.js
@@ -1,39 +1,16 @@
 import { useState } from "react";
+import ReviewBeanRating from './ReviewBeanRating';
 import { TiThumbsUp } from "react-icons/ti";
 import { MdReportGmailerrorred } from "react-icons/md";
 
-const data = [
-  {
-  'firstName': 'Stormi',
-  'date': 'Oct 18, 2021',
-  'default_pic': '',
-  'rating': 4,
-  'title': 'Awesome beans',
-  'body': 'This is place has the best beans. I would recommend this cafe to anyone who loves authentic coffee!',
-  'helpful': 0,
-  'report': 0,
-  'photos': []
-  },{
-  'firstName': 'Stormi',
-  'date': 'Oct 18, 2021',
-  'default_pic': '',
-  'rating': 4,
-  'title': 'Awesome beans',
-  'body': 'This is place has the best beans. I would recommend this cafe to anyone who loves authentic coffee!',
-  'helpful': 0,
-  'report': 0,
-  'photos': []
-  }
-]
-
 export default function Review() {
 
-  const review = data.map((review, index)=> {});
+  // const review = data.map((review, index)=> {});
 
   return (
     <div id="review">
       <div id="review-user-info">
-        <img src="" id=""reviewer-photo/>
+        {/* <img src="" id="reviewer-photo"/> */}
         <div id="review-user-info-inner">
           <div id="reviewer-first-name">first name</div>
           <div id="reviewer-date">date</div>
@@ -41,7 +18,7 @@ export default function Review() {
       </div>
 
       <div id="review-center">
-        <span>user rating</span>
+        <ReviewBeanRating />
         <div id="review-title">title</div>
         <div id="review-body">body</div>
       </div>
