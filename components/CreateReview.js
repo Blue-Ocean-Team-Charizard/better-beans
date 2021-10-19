b63e9cf20d798af18ef51be3f0422c08fab24b28
Qinyu Zhu <zz635186056@gmail.com>
2021-10-19 12:02:08 -0700

    update form

--- components/CreateReview.js
+++ components/CreateReview.js
@@ -1,12 +1,41 @@
-import CreateReviewForm from './CreateReviewForm';
+import { useState } from 'react';
 
 export default function CreateReview() {
+  const [body, setBody] = useState('');
+  const [photo, setPhoto] = useState([]);
+
+  const handleSubmit = () => {
+    axios.post(`the post API`, {params: `params`})
+      .then(response => {
+        return (
+          console.log('added')
+        )
+      })
+      .catch(err => console.log('add review err', err));
+  }
+
+  const handleImage = (e) => {
+    e.preventDefault();
+    if(e.target.files) {
+      let file = Array.from
+    }
+  }
+
   return (
-
-
-    // the modal component should be adding options
+    // the modal component should be adding option
     <div>
-      <h2>Create Review</h2>
+      <div id='review'>
+        <form onSubmit={(e) => {e.preventDefault(); handleSubmit();}}>
+          <label>
+            Write your reviews down
+          </label>
+          <br/>
+          <label>
+            Your photos(optional)
+          </label>
+          <button type='submit'> Submit Review</button>
+        </form>
+      </div>
     </div>
   );
 }
