a1caedafdadd239b32169b2cab9ffc3ccfa51e01
Ya Liu <sarahl0216@gmail.com>
2021-10-18 21:58:20 -0700

    refactored search component to populate shops data in state

--- components/Search.jsx
+++ components/Search.jsx
@@ -8,34 +8,34 @@ export default class Search extends React.Component {
     };
 
     this.searchCurrentLocation = this.searchCurrentLocation.bind(this);
+    this.geoSuccess = this.geoSuccess.bind(this);
   }
 
   componentDidMount() {
     this.searchCurrentLocation();
   }
 
+  geoSuccess(pos) {
+    const crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    // this.setState({ currentLocation: crd });
+    // 34.05223, -118.24368
+    fetch(`/api/search?location=${crd.latitude},${crd.longitude}`)
+      .then((res) => res.json())
+      .then((data) => this.setState({ shops: data }))
+      .catch((err) => console.log(err));
+  }
+
   searchCurrentLocation() {
     if (navigator.geolocation) {
-      function success(pos) {
-        const crd = pos.coords;
-
-        console.log('Your current position is:');
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        // this.setState({ currentLocation: crd });
-        // 34.05223, -118.24368
-        fetch(`/api/search?location=${crd.latitude},${crd.longitude}`)
-          .then((data) => this.setState({ shops: data }))
-          .catch((err) => console.log(err))
-      }
-
-      function error(err) {
+      navigator.geolocation.getCurrentPosition(this.geoSuccess, (err) => {
         console.warn(`ERROR(${err.code}): ${err.message}`);
-      }
-
-      navigator.geolocation.getCurrentPosition(success, error);
+      });
     } else {
-      console.log('location is not enabled')
+      console.log('location is not enabled');
     }
   }
 
@@ -44,7 +44,7 @@ export default class Search extends React.Component {
       <div className="search">
         <input type="text" id="search" placeholder="Search location" />
         <button type="submit">GO</button>
-        <button onClick={this.searchCurrentLocation}>Search Near Me</button>
+        <button type="button" onClick={this.searchCurrentLocation}>Search Near Me</button>
       </div>
     );
   }
--- pages/api/search.js
+++ pages/api/search.js
@@ -1,11 +1,11 @@
 import getPlaces from '../../helpers/nearbySearch';
 
 export default function handler(req, res) {
-  const chainNames = ['Starbucks', 'Peet\'s Coffee', 'Dunkin\'', 'McDonald\'s', 'Subway', 'Denny\'s', 'Yum Yum Donuts', 'Blue Bottle Coffee', 'The Coffee Bean & Tea Leaf', 'LavAzza'];
+  const chainNames = ['Starbucks', 'Peet\'s Coffee', 'Dunkin\'', 'McDonald\'s', 'Subway', 'Denny\'s', 'Yum Yum Donuts', 'Blue Bottle Coffee', 'The Coffee Bean & Tea Leaf', 'LavAzza', 'Einstein Bros. Bagels'];
   getPlaces(req.query.location)
     .then((places) => {
       const filtered = places.results.filter((place) => (chainNames.indexOf(place.name) === -1));
-      console.log(filtered);
+      // console.log(filtered);
       res.status(200).json(filtered);
     })
     .catch((err) => {
