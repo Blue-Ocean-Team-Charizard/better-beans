9567c1e10550b41ebdb3691001338c7f0cd7b113
Ya Liu <sarahl0216@gmail.com>
2021-10-18 17:35:47 -0700

    implemented filters on coffee shop chains

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,10 +1,12 @@
 import getPlaces from '../../helpers/nearbySearch';
 
 export default function handler(req, res) {
+  const chainNames = ['Starbucks', 'Peet\'s Coffee', 'Dunkin\'', 'McDonald\'s', 'Subway', 'Denny\'s'];
   getPlaces(req.query.location)
     .then((places) => {
-      console.log(places);
-      res.status(200).json(places);
+      const filtered = places.results.filter((place) => (chainNames.indexOf(place.name) === -1));
+      console.log(filtered);
+      res.status(200).json(filtered);
     })
     .catch((err) => {
       console.log(err);
