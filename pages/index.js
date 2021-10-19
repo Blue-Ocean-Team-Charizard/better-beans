f87127cf52a50d690f0f92da28567b9e58acc758
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-10-19 11:57:35 -0700

    Corrections made to previous PR

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-import Head from 'next/head';
-import Image from 'next/image';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
-=======
 import Meta from "../components/Meta";
 // import Shop from "../components/Shop";
->>>>>>> main
 export default function Home() {
   return (
     <div>
