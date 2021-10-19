f65a28e3c12ed37c9cf1513b9f640e13b328f87b
Van Nguyen <van.nguyen@texo.design>
2021-10-19 12:20:28 -0700

    Add the shopList

---
+++ pages/search/index.js
@@ -0,0 +1,10 @@
+import ShopList from '../../components/ShopList';
+
+export default function Search() {
+  return (
+    <div>
+      <h1>Shops near me </h1>
+      <ShopList />
+    </div>
+  );
+}
