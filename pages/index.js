82ee1b698bba0bf9237962dc7c3faadbc9db9d97
Izzy Lee <86844794+isabell-lee@users.noreply.github.com>
2021-10-20 11:07:31 -0700

    add Meta back in

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Meta from "../components/Meta";
 
 export default function Home() {
   return (
-    <div id="home">
+    <div id="home>
+      <Meta />
+      <h1>Start Finding Local Coffee Shops Today!</h1>
     </div>
   );
 }
