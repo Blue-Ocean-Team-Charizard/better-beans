0c7552e80f40b4e114509dc704e7af10b57b6b13
Ya Liu <sarahl0216@gmail.com>
2021-10-20 10:42:19 -0700

    wrapped search context around shop/[id]/index.js so each shop receives google data

--- pages/shop/[id]/index.js
+++ pages/shop/[id]/index.js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import Meta from '../../../components/Meta';
 import Shop from '../../../components/Shop';
+import { SearchContext } from '../../../components/SearchContext';
 
 export default function Shops(props) {
   const router = useRouter();
@@ -14,7 +15,11 @@ export default function Shops(props) {
       {id} */}
       {/* {console.log(shop)}; */}
       {/* {console.log('props', props)}; */}
-      <Shop googleData={props.googleData} reviews={props.reviews} />
+      <SearchContext.Consumer>
+        {(context) => (
+          <Shop googleData={context.selectedShop} reviews={props.reviews} />
+        )}
+      </SearchContext.Consumer>
     </div>
   );
 }
