467698f7e0d3cb008a93828cd88028b07de97f1d
Van Nguyen <van.nguyen@texo.design>
2021-10-20 11:16:04 -0700

    fixed error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Meta from "../components/Meta";
 
 export default function Home() {
   return (
-    <div id="home>
+    <div id="home">
       <Meta />
       <h1>Start Finding Local Coffee Shops Today!</h1>
     </div>
