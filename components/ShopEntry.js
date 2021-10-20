d40e5fa9f15eb4b3e0c9e7813598a2eeb36e8b5c
Daniel Prejs <danielnprejs@gmail.com>
2021-10-19 17:15:36 -0700

    map refresh location and shoplist opening hours fixed

--- components/ShopEntry.js
+++ components/ShopEntry.js
@@ -8,8 +8,12 @@ const ShopEntry = ({ shop }) => {
       <Link href="#">
         <a className="">
           <h3 className="name">{shop.name}</h3>
-          <BeanRating rating={shop.rating}/>
-          <div className="opening_hours">{shop.opening_hours.open_now}</div>
+          <BeanRating rating={shop.rating} />
+          {shop.opening_hours ?
+          shop.opening_hours.open_now ? <div className="opening_hours">Open</div> :
+          <div className="opening_hours">Closed</div> :
+          null}
+          {/* <div className="opening_hours">{shop.opening_hours.open_now}</div> */}
           <div className="location">{shop.location}</div>
         </a>
       </Link>
