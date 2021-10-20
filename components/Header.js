a1a1e0eb91334fc5830fe5011db68a591bcc0a79
Ya Liu <sarahl0216@gmail.com>
2021-10-20 16:30:23 -0700

    fixed font awesome bug, icons now render, favicon now renders fixed some code style according to eslint

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import Search from './Search';
 import Head from 'next/head';
+import Search from './Search';
 import { SearchContext } from './SearchContext';
 
 export default function Header() {
@@ -8,12 +8,10 @@ export default function Header() {
     <>
       <Head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        {/* <script src="https://kit.fontawesome.com/02899377c4.js" crossorigin="anonymous"></script> */}
-        <script defer src="https://pro.fontawesome.com/releases/v5.10.0/js/all.js" integrity="sha384-G/ZR3ntz68JZrH4pfPJyRbjW+c0+ojii5f+GYiYwldYU69A+Ejat6yIfLSxljXxD" crossorigin="anonymous"></script>
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
+        <link href="https://use.fontawesome.com/releases/v5.0.2/css/all.css" rel="stylesheet"></link>
+        {/* <script src="https://kit.fontawesome.com/02899377c4.js" crossOrigin="anonymous"></script> */}
       </Head>
-
       <header>
         <div className="navbar navbar-dark shadow-sm">
           <div id="nav">
