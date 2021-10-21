a3f11fcc0728a75b066081d76787b2ee06a4406f
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-10-20 17:30:41 -0700

    Moved sign out btn to bottom of profile page, added a delete account feature, login/profile btn dynamic on where you are signed in or not

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -5,7 +5,7 @@ import Meta from '../../components/Meta';
 import ReviewList from '../../components/ReviewList';
 
 export default function Profile() {
-  const { authUser, loading, logOff } = useAuth();
+  const { authUser, loading, logOff, deleteAccount } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
@@ -22,21 +22,15 @@ export default function Profile() {
           {(authUser) ? <img className="profile-photo" src={authUser.photo} alt="avatar" /> : null}
         </div>
         <h3 className="info name">{(authUser) ? ` ${authUser.name}` : ''}</h3>
-        <div className="btnDelete">Delete account</div>
       </div>
-      <h1 className="title">Review
-        <button type="button" className="btn btn-review"> Write a Review</button>
+      <h1 className="title">
+        Reviews
       </h1>
 
-
-      <button
-        type="button"
-        onClick={logOff}
-      >
-        Log Off!
-      </button>
       <br />
       <ReviewList />
+      <button type="button" className="btn" onClick={logOff}>Sign Out</button>
+      <button type="button" className="btn" onClick={deleteAccount}>Delete Account</button>
     </div>
   );
 }
