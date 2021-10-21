f740beb8579419bb4baa66d8f604c073741a6b1c
Van Nguyen <van.nguyen@texo.design>
2021-10-20 20:44:26 -0700

    Add video for home page

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,19 @@ export default function Home() {
   const [isPlaying, setIsPlaying] = useState(true);
 
   return (
-    <div id="home">
+    <>
       <Meta />
       <HomePageID />
-      <h1>Start Finding Local Coffee Shops Today!</h1>
-      <ReactPlayer url="https://css-tricks-post-videos.s3.us-east-1.amazonaws.com/blurry-trees.mov" playing={ isPlaying } />
-      {/* <div class="video">
-        <video src="https://css-tricks-post-videos.s3.us-east-1.amazonaws.com/blurry-trees.mov" autoplay loop playsinline muted></video>
-      </div> */}
-    </div>
+      <h1>Start Finding <br />Local Coffee Shops Today!</h1>
+      <div className="over"></div>
+      <ReactPlayer url="../production ID_4820352.mp4" playing="true" loop="true" />
+      {/* <ReactPlayer url="../production ID_4820350.mp4" playing="true" loop="true" /> */}
+      {/* <ReactPlayer url="../production ID_4820349.mp4" playing="true" loop="true" /> */}
+      {/* <ReactPlayer url="../production ID_4815783.mp4" playing="true" loop="true" /> */}
+      {/* <ReactPlayer url="../production ID_4498416.mp4" playing="true" loop="true" /> */}
+      {/* <ReactPlayer url="../pexels-mike-m-9063027.mp4" playing="true" loop="true" /> */}
+      {/* <ReactPlayer url="../pexels-anthony-shkraba-7174275.mp4" playing="true" loop="true" /> */}
+      {/* <ReactPlayer url="../pexels-nicola-barts-7930831.mp4" playing="true" loop="true" /> */}
+    </>
   );
 }
-
-
