323cbbd7a08dd65070c0b9391cea83a874ca1bc9
Van Nguyen <van.nguyen@texo.design>
2021-10-20 19:10:02 -0700

    Add video for hompage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Meta from "../components/Meta";
+import { useState } from 'react';
 import HomePageID from "../components/AddHomeId";
+import ReactPlayer from 'react-player';
 
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useState(true);
+
   return (
     <div id="home">
       <Meta />
       <HomePageID />
       <h1>Start Finding Local Coffee Shops Today!</h1>
+      <ReactPlayer url="https://css-tricks-post-videos.s3.us-east-1.amazonaws.com/blurry-trees.mov" playing={ isPlaying } />
+      {/* <div class="video">
+        <video src="https://css-tricks-post-videos.s3.us-east-1.amazonaws.com/blurry-trees.mov" autoplay loop playsinline muted></video>
+      </div> */}
     </div>
   );
 }
+
+
