9b4cdaa3233c2836910d4a13b9c8a97a43177c1a
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2021-10-21 16:23:43 -0700

    Finished styling btns, made all buttons jsx component

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,6 +1,9 @@
 import { useAuth } from '../../firebase/auth_context';
 import Meta from '../../components/Meta';
 import LoginPageID from '../../components/AddLoginId';
+import GoogleSignIn from '../../components/GoogleSignIn'
+import FacebookSignIn from '../../components/FacebookSignIn'
+import GithubSignIn from '../../components/GithubSignIn'
 
 export default function LoginPage() {
   const { signInWithFirebase, signInWithFacebook, signInWithGithub } = useAuth();
@@ -11,64 +14,9 @@ export default function LoginPage() {
       <div className="login-btn-container">
         <div id="login">
           <p>Sign up today to save your better <br /> and best beans</p>
-          <button
-            type="button"
-            className="btn-google"
-            onClick={signInWithFirebase}
-          >
-            <div className="google-btn">
-              <img
-                className="google-logo"
-                src="/google_logo.png"
-                alt="google"
-                width="25px"
-                height="25px"
-              />
-              <h3 className="google-signin">
-                Continue with Google
-              </h3>
-            </div>
-          </button>
-
-
-          <button
-            type="button"
-            className="btn-facebook"
-            onClick={signInWithFacebook}
-          >
-            <div className="facebook-btn">
-              <img
-                className="facebook-logo"
-                src="/facebook_logo.png"
-                alt="facebook"
-                width="25px"
-                height="25px"
-              />
-              <h3 className="facebook-signin">
-                Continue with Facebook
-              </h3>
-            </div>
-          </button>
-
-
-          <button
-            type="button"
-            className="btn-github"
-            onClick={signInWithGithub}
-          >
-            <div className="google-btn">
-              <img
-                className="github-logo"
-                src="/github_logo.png"
-                alt="github"
-                width="25px"
-                height="25px"
-              />
-              <h3 className="github-signin">
-                Continue with Github
-              </h3>
-            </div>
-          </button>
+          <GoogleSignIn />
+          <FacebookSignIn />
+          <GithubSignIn />
         </div>
       </div>
     </>
