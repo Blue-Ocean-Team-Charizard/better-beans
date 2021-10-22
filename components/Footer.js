bd4b816f3e407b470d4696bcdda3e010027091c2
Jandean Deocampo <jandeo92@gmail.com>
2021-10-22 15:36:37 -0700

    Moved position of egg.

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,7 @@
-import Link from 'next/link';
-
 export default function Footer() {
   return (
     <footer className="">
       <p> Copyright © by Team Charizard</p>
-      <button
-        style={
-          {
-            backgroundColor: "#0f0f2f",
-            cursor: "pointer",
-            position: "absolute",
-            width: "3rem",
-            left: "2%",
-            borderRadius: ".8rem",
-            border: "none",
-            fontSize: ".5rem",
-            fontFamily: "Bitter",
-          }
-        }
-      >
-        <Link href="easteregg">
-          freesub
-        </Link>
-      </button>
     </footer>
   );
 }
--- pages/easteregg.js
+++ pages/easteregg.js
@@ -1,7 +1,7 @@
 export default function EasterEgg () {
   return (
     <iframe width="100%" height="800px"
-      src="https://www.youtube.com/embed/M_ThtCRAySw">
+      src="https://www.youtube.com/embed/rg6CiPI6h2g">
     </iframe>
   );
 };
