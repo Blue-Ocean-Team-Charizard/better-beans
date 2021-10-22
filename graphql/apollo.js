43d4f4dba99114763950d2e2e935a46b51699064
Jandean Deocampo <jandeo92@gmail.com>
2021-10-21 23:51:39 -0700

    Updated Apollo uri.

--- graphql/apollo.js
+++ graphql/apollo.js
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 // https://better-beans.vercel.app
+// http://localhost:3000
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: 'https://better-beans.vercel.app/api/graphql',
   cache: new InMemoryCache(),
 });
 
