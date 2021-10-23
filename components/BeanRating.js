d8349d44d7e10d18d32e20378ddfb6a09dca8960
Izzy Lee <86844794+isabell-lee@users.noreply.github.com>
2021-10-23 10:09:34 -0700

    removed console logs

--- components/BeanRating.js
+++ components/BeanRating.js
@@ -4,17 +4,14 @@ export default function BeanRating({ rating, reviews }) {
     let totalRatings = 0;
     reviews.map((review) => {
       totalRatings += review.rating;
-      console.log(totalRatings);
       return null;
     });
     usedRating = totalRatings / reviews.length;
-    console.log(usedRating);
   };
 
   const convertedRating = (usedRating / 5) * 100;
   const roundedRating = (Math.round(convertedRating / 5) * 5);
   const style = { width: `${roundedRating}%` };
-  console.log(roundedRating);
   return (
     <>
       <div className="beans-outer" >
