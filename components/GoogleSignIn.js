079d8311758ae24179b009ccead8d95363381b2f
Jesus Gonzales <jesus.gonzales1995@gmail.com>
2022-03-01 21:56:29 -0800

    Google maps works locally

--- components/GoogleSignIn.js
+++ components/GoogleSignIn.js
@@ -4,22 +4,22 @@ export default function GoogleSignIn() {
   const { signInWithFirebase } = useAuth();
   return (
     <button
-            type="button"
-            className="btn-google"
-            onClick={signInWithFirebase}
-          >
-            <div className="google-btn">
-              <img
-                className="google-logo"
-                src="/google_logo.png"
-                alt="google"
-                width="25px"
-                height="25px"
-              />
-              <h3 className="google-signin">
-                Continue with Google
-              </h3>
-            </div>
-          </button>
+      type="button"
+      className="btn-google"
+      onClick={signInWithFirebase}
+    >
+      <div className="google-btn">
+        <img
+          className="google-logo"
+          src="/google_logo.png"
+          alt="google"
+          width="25px"
+          height="25px"
+        />
+        <h3 className="google-signin">
+          Continue with Google
+        </h3>
+      </div>
+    </button>
   )
 }
